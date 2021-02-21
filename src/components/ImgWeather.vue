6a80a89239aa44778b0438566b70eb1bd2a83072
KillEverybody <Ambd@ukr.net>
2021-02-21 03:14:28 +0200

    Created img component and update JSON

---
+++ src/components/ImgWeather.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src='getIcon' alt=''>
+</template>
+
+<script>
+export default {
+  name: 'WtpImgWeather',
+  props: {
+    weatherImg: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    getIcon() {
+      return `http://openweathermap.org/img/wn/${this.weatherImg}@2x.png`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
